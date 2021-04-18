3162e469197dcdbded2458a704cea5aa8034602e
Cyril Lagger <lagger.cyril@gmail.com>
2021-04-18 10:12:44 +0100

    Fixed dblclick redirection

--- shinyApp/www/scAgeCom.js
+++ shinyApp/www/scAgeCom.js
@@ -23,9 +23,9 @@ $(document).ready(() => {
 //    #### TCA_GLOBAL_TABLE events ####
 // https://stackoverflow.com/questions/38797646/hyperlink-from-one-datatable-to-another-in-shiny
 
-const TCA_GLOBAL_TABLE_term_selector = "div[data-value='TCA_SUMMARY_TABLE'] #TCA_GLOBAL_DETAILS tr td:nth-child(2)";
+const TCA_GLOBAL_TABLE_term_selector = "#TCA_GLOBAL_DETAILS tbody tr td:nth-child(2)";
 const TCA_KEYWORD_SUMMARY_selector = "a[data-value='TCA_KEYWORD_SUMMARY']";
-const TCA_GLOBAL_TABLE_CATEGORY_CHOICE_selector = "input#TCA_GLOBAL_TABLE_CHOICE-selectized";
+//const TCA_GLOBAL_TABLE_CATEGORY_CHOICE_selector = "input#TCA_GLOBAL_TABLE_CHOICE-selectized";
 const TCA_KEYWORD_SUMMARY_CATEGORY_CHOICE_selector = "input#TCA_KEYWORD_CATEGORY_CHOICE-selectized";
 const TCA_KEYWORD_VALUE_CATEGORY_SERVER_INPUT_ID = "TCA_KEYWORD_CATEGORY_CHOICE";
 const TCA_KEYWORD_VALUE_CHOICE_SERVER_INPUT_ID = "TCA_KEYWORD_VALUE_CHOICE_JS_TRIGGERED";
@@ -35,15 +35,15 @@ const TIMEOUT = 1000; // ms
 function TCA_GLOBAL_TABLE_term_click_handler() {
   let cell = $(this);
   let value = cell.text();
-  let category = $(TCA_GLOBAL_TABLE_CATEGORY_CHOICE_selector).siblings('div').text();
-
+  let tableid = $(this).closest('table').attr('id');
+  let category = $("#" + tableid + " thead tr:nth-child(1) th:nth-child(2)").text();
+  //let category = $(TCA_GLOBAL_TABLE_CATEGORY_CHOICE_selector).siblings('div').text();
   $(TCA_KEYWORD_SUMMARY_selector).trigger('click');
-
   let category_in_TCA_KEYWORD = $(TCA_KEYWORD_SUMMARY_CATEGORY_CHOICE_selector).siblings('div').text();
+
   if (category != category_in_TCA_KEYWORD) {
       Shiny.setInputValue(TCA_KEYWORD_VALUE_CATEGORY_SERVER_INPUT_ID, category, {priority: "event"});
   }
-
   setTimeout(() => {
     Shiny.setInputValue(TCA_KEYWORD_VALUE_CHOICE_SERVER_INPUT_ID, value, {priority: "event"});
   }, TIMEOUT);
@@ -66,16 +66,17 @@ $(document).on("mouseenter", TCA_GLOBAL_TABLE_term_selector, () => {
 }); */
 
 // Heatmap to TSA redirection
-const TCA_KEYWORD_HEATMAP_selector = "#TCA_KEYWORD_SUMMARY";
+const TCA_KEYWORD_HEATMAP_selector = "#TCA_KEYWORD_SUMMARY_PLOT";
 
-$(TCA_KEYWORD_SUMMARY_selector).once(() => {
-  $(document).on("plotly_click", TCA_KEYWORD_HEATMAP_selector, function(dummy, data) {
-    //console.log(data);
-    let txt = data.points[0].data.text;
-    let dataset = txt.match(/DATASET: (.*)<br/)[1];
-    let tissue = txt.match(/TISSUE: (.*)$/)[1];
-    console.log(dataset, tissue);
-  });
-});
+//$(TCA_KEYWORD_SUMMARY_selector).once(() => {
+//$(document).on("plotly_click", TCA_KEYWORD_HEATMAP_selector, function(dummy, data) {
+//    console.log(data);
+//    let txt = data.points[0].data.text;
+//    console.log(txt)
+//    let dataset = txt.match(/DATASET: (.*)<br/)[1];
+//    let tissue = txt.match(/TISSUE: (.*)$/)[1];
+//    console.log(dataset, tissue);
+//  });
+//});
 
 //    #################################
