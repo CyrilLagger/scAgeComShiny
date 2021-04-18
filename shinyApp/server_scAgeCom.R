f9a0bd8910a7722b12182239a738034db1c7ec69
Cyril Lagger <lagger.cyril@gmail.com>
2021-04-18 10:09:38 +0100

    Fixed typo

--- shinyApp/server_scAgeCom.R
+++ shinyApp/server_scAgeCom.R
@@ -3,7 +3,7 @@ server_scAgeCom <- function(
   output,
   session
 ) {
-  output$MAIN_TITLE <- renderText({
+  output$MAIN_TITLE <- renderUI({
     scAgeCom_data$shiny_html_content$main_title
   })
   source("utils_introduction.R", local = TRUE)
