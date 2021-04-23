72db2585ecd8d8ef90d7ff79433185ee64adb462
Cyril Lagger <lagger.cyril@gmail.com>
2021-04-23 16:17:26 +0100

    Updated data

---
+++ R/data.R
@@ -0,0 +1,13 @@
+#' The scAgeCom data to be displayed on the ShinyApp
+#'
+#'
+#' @docType data
+#'
+#' @usage data(scAgeCom_data)
+#'
+#' @format A list
+#'
+#' @keywords datasets
+#'
+#'
+"scAgeCom_data"
