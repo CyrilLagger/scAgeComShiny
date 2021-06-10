32a6d7e1c510a43bc9c6a219e6697a5a148d791a
Cyril Lagger <lagger.cyril@gmail.com>
2021-06-10 16:34:45 +0100

    Removed uncessary option

--- app.R
+++ app.R
@@ -4,6 +4,5 @@
 
 pkgload::load_all(export_all = FALSE,helpers = FALSE,attach_testthat = FALSE)
 options( "golem.app.prod" = TRUE)
-#options(htmlwidgets.TOJSON_ARGS = list(na = 'string'))
-options(DT.TOJSON_ARGS = list(na = 'string'))
+#options(DT.TOJSON_ARGS = list(na = 'string'))
 scAgeComShiny::run_app() # add parameters here (if any)
