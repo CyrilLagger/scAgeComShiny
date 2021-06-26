78c6373744b5a81021a3780ee4079dc1b139b4c4
Cyril Lagger <lagger.cyril@gmail.com>
2021-06-26 14:59:28 +0100

    Restore NA display

--- app.R
+++ app.R
@@ -2,7 +2,7 @@
 # To deploy, run: rsconnect::deployApp()
 # Or use the blue button on top of this file
 
-pkgload::load_all(export_all = FALSE,helpers = FALSE,attach_testthat = FALSE)
-options( "golem.app.prod" = TRUE)
-#options(DT.TOJSON_ARGS = list(na = 'string'))
+pkgload::load_all(export_all = FALSE, helpers = FALSE, attach_testthat = FALSE)
+options("golem.app.prod" = TRUE)
+options(DT.TOJSON_ARGS = list(na = "string"))
 scAgeComShiny::run_app() # add parameters here (if any)
