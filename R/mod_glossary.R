a48f8d1d2b7b274fd297f9a779b072e1306f25b5
Cyril Lagger <lagger.cyril@gmail.com>
2021-06-29 14:19:57 +0100

    Add some glossary terminology

--- R/mod_glossary.R
+++ R/mod_glossary.R
@@ -41,6 +41,42 @@ mod_glossary_server <- function(id){
             width = 8,
             htmlOutput(ns("GLOSSARY_CONTENT_GT"))
           )
+        ),
+        fluidRow(
+          column(
+            width = 8,
+            titlePanel(htmlOutput(ns("GLOSSARY_TITLE_DATA"))),
+          )
+        ),
+        fluidRow(
+          column(
+            width = 8,
+            htmlOutput(ns("GLOSSARY_CONTENT_DATA"))
+          )
+        ),
+        fluidRow(
+          column(
+            width = 8,
+            titlePanel(htmlOutput(ns("GLOSSARY_TITLE_DA"))),
+          )
+        ),
+        fluidRow(
+          column(
+            width = 8,
+            htmlOutput(ns("GLOSSARY_CONTENT_DA"))
+          )
+        ),
+        fluidRow(
+          column(
+            width = 8,
+            titlePanel(htmlOutput(ns("GLOSSARY_TITLE_ORA"))),
+          )
+        ),
+        fluidRow(
+          column(
+            width = 8,
+            htmlOutput(ns("GLOSSARY_CONTENT_ORA"))
+          )
         )
       )
     })
@@ -50,38 +86,54 @@ mod_glossary_server <- function(id){
         style = paste(
           "font-size: 22px;"
         ),
-        "General terminology"
+        "General Terminology"
       )
     })
     
     output$GLOSSARY_CONTENT_GT <- renderUI({
       bsplus::bs_accordion(
-        id = "HELP_GLOSSARY"
+        id = "HELP_GT"
       ) %>%
         bsplus::bs_append(
           title = "LRI: ligand-receptor interaction",
           content = paste(
-            "TODO"
+            "A LRI is formally defined as a pair of ligand gene(s)",
+            "and receptor gene(s) coding for a known protein-protein",
+            "interaction. It can be simple, namely involving a single ligand",
+            "gene and receptor gene (e.g. Apoe:Ldlr), or complex if",
+            "involving an heteromeric complex (e.g. Col3a1:Itgb1_Itga2)."
           )
         ) %>%
         bsplus::bs_append(
           title = "CCI: cell-cell interaction",
           content = paste(
-            "TODO"
+            "A communication signals between two cell types mediated by a",
+            "ligand-receptor interaction. For example, (B cell, T cell, Apoe:Ldlr)",
+            "is a CCI that corresponds to the emission of Apoe from B cells and that",
+            "will interact with Ldlr expressed by T cells"
           )
         ) %>%
         bsplus::bs_append(
-          title = "ORA Score",
+          title = "TODO",
           content = paste(
             "TODO"
           )
-        ) %>%
-        bsplus::bs_append(
-          title = "ORA Score",
-          content = paste(
-            "TODO"
-          )
-        ) %>%
+        )
+    })
+    
+    output$GLOSSARY_TITLE_DATA <- renderUI({
+      tags$p(
+        style = paste(
+          "font-size: 22px;"
+        ),
+        "scRNA-seq dataset Terminology"
+      )
+    })
+    
+    output$GLOSSARY_CONTENT_DATA <- renderUI({
+      bsplus::bs_accordion(
+        id = "HELP_DATA"
+      ) %>%
         bsplus::bs_append(
           title = "TMS FACS",
           content = paste(
@@ -99,5 +151,72 @@ mod_glossary_server <- function(id){
           )
         )
     })
+    
+    output$GLOSSARY_TITLE_DA <- renderUI({
+      tags$p(
+        style = paste(
+          "font-size: 22px;"
+        ),
+        "Detection/differential analysis Terminology"
+      )
+    })
+    
+    output$GLOSSARY_CONTENT_DA <- renderUI({
+      bsplus::bs_accordion(
+        id = "HELP_DA"
+      ) %>%
+        bsplus::bs_append(
+          title = "Detected CCIs",
+          content = paste(
+            "TODO"
+          )
+        ) %>%
+        bsplus::bs_append(
+          title = "UP/DOWN/FLAT/NSC CCIs",
+          content = paste(
+            "TODO"
+          )
+        )
+    })
+    
+    output$GLOSSARY_TITLE_ORA <- renderUI({
+      tags$p(
+        style = paste(
+          "font-size: 22px;"
+        ),
+        "Over-representation analysis Terminology"
+      )
+    })
+    
+    output$GLOSSARY_CONTENT_ORA <- renderUI({
+      bsplus::bs_accordion(
+        id = "HELP_ORA"
+      ) %>%
+        bsplus::bs_append(
+          title = "ORA",
+          content = paste(
+            ""
+          )
+        )  %>%
+        bsplus::bs_append(
+          title = "Odds Ratio",
+          content = paste(
+            ""
+          )
+        )  %>%
+        bsplus::bs_append(
+          title = "Adj. p-value",
+          content = paste(
+            ""
+          )
+        )  %>%
+        bsplus::bs_append(
+          title = "ORA score",
+          content = paste(
+            ""
+          )
+        )
+    })
+    
   })
 }
