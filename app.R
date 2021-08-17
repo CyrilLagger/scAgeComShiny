81e7d63581830470e462a48d60ad5ab32ddd1181
Cyril Lagger <lagger.cyril@gmail.com>
2021-08-17 12:22:07 +0100

    Remove global option

--- app.R
+++ app.R
@@ -4,5 +4,4 @@
 
 pkgload::load_all(export_all = FALSE, helpers = FALSE, attach_testthat = FALSE)
 options("golem.app.prod" = TRUE)
-options(DT.TOJSON_ARGS = list(na = "string"))
 scAgeComShiny::run_app() # add parameters here (if any)
